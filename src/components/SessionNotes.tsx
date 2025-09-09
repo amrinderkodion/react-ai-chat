815443aa80cac5d952a8e38ad8f36a724680f031
Amrinder Kodion <amrinder01.kodion@gmail.com>
2025-09-09 11:51:20 +0530

    Replace summernote with CKEditor for session notes editing. Update package.json and package-lock.json to reflect new CKEditor dependencies and remove summernote-related packages.

--- src/components/SessionNotes.tsx
+++ src/components/SessionNotes.tsx
@@ -1,7 +1,8 @@
 // components/SessionNotes.tsx
 import * as React from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 
 interface SessionNotesProps {
     notes: string;
@@ -9,16 +10,16 @@ interface SessionNotesProps {
     styling?: React.CSSProperties;
 }
 
-export default function SessionNotes({ notes, onNotesChange, styling }: SessionNotesProps) {
+export default function SessionNotes({ notes, onNotesChange, styling = {} }: SessionNotesProps) {
     return (
-        <Box sx={{width: '100%', ...styling }}>
-            <TextField
-                label="Session Notes"
-                multiline
-                minRows={4}
-                value={notes}
-                onChange={e => onNotesChange(e.target.value)}
-                sx={{bgcolor: '#fff', width: '100%'}}
+        <Box sx={{ flex: 1, ...styling }}>
+            <CKEditor
+                editor={ClassicEditor}
+                data={notes}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    onNotesChange(data);
+                }}
             />
         </Box>
     );
